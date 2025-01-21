7be4e77dc1147688ccd980febb2c8879f87ac84e
Vicky197709 <vickyituze@gmail.com>
2025-01-21 15:11:03 +0200

    Effectuer sur mon phone

--- HelloWord.java
+++ HelloWord.java
@@ -3,5 +3,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("hello world...");
         System.out.println("bonjour tout le monde ");
+        System.out.println("commit effectuer sur phone");
     }
 }
